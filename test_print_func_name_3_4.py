15f14d9f51d740114eaabd868306a1ef3464b928
KseniiaTr <ks31993@mail.ru>
2022-12-29 00:40:25 -0800

    Добавила отступ

--- test_print_func_name_3_4.py
+++ test_print_func_name_3_4.py
@@ -14,6 +14,7 @@ def go_to_companyname_homepage(page_url):
 def find_registration_button_on_login_page(page_url, button_text):
     print_name(find_registration_button_on_login_page.__name__, page_url, button_text)
 
+
 open_browser("Chrome")
 go_to_companyname_homepage("Google.com")
 find_registration_button_on_login_page("Google.com", "Log In")
